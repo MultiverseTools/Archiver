c92115ab12753079db179657e8ea68cf35c2d303
heartzwh <541131938@qq.com>
2021-06-25 00:44:49 +0800

    初步完成

--- Example/PlayerInfo.cs
+++ Example/PlayerInfo.cs
@@ -2,21 +2,60 @@
 // Author:      Sora
 // CreateTime:  2021-06-23-17:38
 
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace EFAS.Archiver.Example
 {
     [Archiver("ExamplePlayerInfo")]
+    [ArchiverUpgrade("0.0.0.1", nameof(VersionUpdate1))]
+    // [ArchiverUpdate(2, nameof(VersionUpdate2))]
     public class PlayerInfo
     {
+        /// <summary>
+        /// 1 版本过期
+        /// </summary>
+        // [NonSerialized]
+        [Obsolete]
         public int Hp;
 
+        /// <summary>
+        /// 2 版本过期
+        /// </summary>
+        // [JsonIgnore]
+        // [Obsolete]
         public bool Trigger;
 
         public List<Enemy> Enemies;
 
         [JsonIgnore]
         public int Counter;
+
+        public HpInfo HpInfo;
+
+        // public int TriggerState;
+
+        public static PlayerInfo VersionUpdate1(PlayerInfo _source)
+        {
+            return new PlayerInfo
+            {
+                Trigger = _source.Trigger,
+                Enemies = new List<Enemy>(_source.Enemies),
+                HpInfo = new HpInfo()
+                {
+                    Hp        = _source.Hp,
+                    TestValue = UnityEngine.Random.Range(0, 10000),
+                },
+            };
+        }
+
+        // public void VersionUpdate2() { TriggerState = Trigger ? 999 : 0; }
+    }
+
+    public struct HpInfo
+    {
+        public int Hp;
+        public int TestValue;
     }
 }
---
+++ Scripts/ArchiverUpgradeAttribute.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) 2015 Multiverse
+// Author:      Sora
+// CreateTime:  2021-06-24-12:21
+
+using System;
+
+namespace EFAS.Archiver
+{
+    /// <summary>
+    /// 存档升级
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true, Inherited = false)]
+    public class ArchiverUpgradeAttribute : Attribute
+    {
+        /// <summary>
+        /// 版本号
+        /// </summary>
+        public readonly string Version;
+
+        /// <summary>
+        /// 升级使用方法
+        /// </summary>
+        public readonly string UpgradeFunctionName;
+
+        /// <summary>
+        /// 更新存档元素名称
+        /// </summary>
+        /// <param name="_version">生效版本号. 小于版本号都需要调用对应的方法</param>
+        /// <param name="_upgradeFunctionName">升级存档调用函数</param>
+        public ArchiverUpgradeAttribute(string _version, string _upgradeFunctionName)
+        {
+            Version            = _version;
+            UpgradeFunctionName = _upgradeFunctionName;
+        }
+    }
+}
