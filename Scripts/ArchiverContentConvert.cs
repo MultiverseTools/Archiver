ce6dce07009222cb3749fe0d7fc33f9017a9fc36
heartzwh <541131938@qq.com>
2021-06-28 13:50:46 +0800

    增加: 过滤非保存对象标签[ArchiverElementAttribute]

---
+++ Scripts/ArchiverContentConvert.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) 2015 Multiverse
+// Author:      Sora
+// CreateTime:  2021-06-28-10:46
+
+using System;
+using System.Reflection;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace EFAS.Archiver
+{
+    /// <summary>
+    /// 存档内容转换
+    /// </summary>
+    public class ArchiverContentConvert : JsonConverter
+    {
+        public override void WriteJson(JsonWriter _writer, object? _value, JsonSerializer _serializer)
+        {
+            if (_value == null)
+            {
+                _writer.WriteNull();
+                return;
+            }
+
+            var valueType = _value.GetType();
+            _writer.WriteStartObject();
+
+            foreach (var fieldInfo in valueType.GetFields(BindingFlags.Instance | BindingFlags.Public))
+            {
+                // 过滤不设置ArchiverElementAttribute的字段
+                if (!fieldInfo.IsDefined(typeof(ArchiverElementAttribute), false)) continue;
+                // 保存字段
+                var fieldValue = fieldInfo.GetValue(_value);
+                _writer.WritePropertyName(fieldInfo.Name);
+                if (fieldValue == null)
+                {
+                    _writer.WriteNull();
+                    Debug.LogError($"\"{valueType.FullName}.{fieldInfo.Name}\"保存null.");
+                }
+                else
+                {
+                    _writer.WriteRawValue(JsonConvert.SerializeObject(fieldValue, _serializer.Formatting));
+                }
+            }
+
+            _writer.WriteEndObject();
+        }
+
+        public override object? ReadJson(JsonReader _reader, Type _objectType, object? _existingValue, JsonSerializer _serializer)
+        {
+            // 反序列化不需要实现
+            throw new NotImplementedException();
+        }
+
+        public override bool CanConvert(Type _objectType) => _objectType.IsDefined(typeof(ArchiverContentAttribute), false) && ArchiverManager.s_processStatus == ArchiverManager.PROCESS_STATUS.SERIALIZE;
+    }
+}
